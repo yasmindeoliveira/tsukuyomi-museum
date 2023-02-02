5ae02476f9438925a58a9418052761170345385e
yasmindeoliveira <yaz.oliveira01@gmail.com>
2023-02-02 18:08:32 +0100

    aggiunti metodi controller Admin, create & update

--- TsukuyomiMuseum/Controllers/AdminController.cs
+++ TsukuyomiMuseum/Controllers/AdminController.cs
@@ -141,5 +141,218 @@ namespace TsukuyomiMuseum.Controllers
             return RedirectToAction("Index");
 
         }
+
+        // ---------------------------------- CreateEvent ----------------------------------
+        [HttpGet]
+        public IActionResult CreateEvent()
+        {
+            using MuseumContext db = new();
+
+            Event Modello = new();
+
+            return View("CreateEvent", Modello);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateEvent(Event formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                using MuseumContext db = new();
+
+                return View("CreateEvent", formData);
+
+            }
+            using (MuseumContext db = new MuseumContext())
+            {
+
+                db.Events.Add(formData);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        // ---------------------------------- UpdateEvent ----------------------------------
+        [HttpGet]
+        public IActionResult UpdateEvent(int id)
+        {
+            using (MuseumContext db = new MuseumContext())
+            {
+                Event evento = db.Events.Where(p => p.EventId == id).FirstOrDefault();
+
+                if (evento == null)
+                {
+                    return NotFound("L'evento cercato non è disponibile");
+                }
+
+                Event Modello = new();
+
+                return View("UpdateEvent", Modello);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateEvent(Event formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                using MuseumContext db = new();
+
+                return View("UpdateEvent", formData);
+            }
+
+            using (MuseumContext db = new MuseumContext())
+            {
+                Event evento = db.Events.Where(p => p.EventId == formData.EventId).FirstOrDefault();
+
+                if (evento != null)
+                {
+                    evento.Name = formData.Name;
+                    evento.Description = formData.Description;
+                    evento.Price = formData.Price;
+                    evento.PhotoUrl = formData.PhotoUrl;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("L'evento non è stato trovato");
+                }
+
+            }
+
+        }
+
+        // ---------------------------------- RemoveEvent ----------------------------------
+        [HttpPost]
+        public IActionResult RemoveEvent(int id)
+        {
+
+            using (MuseumContext db = new MuseumContext())
+            {
+                Event evento = (from p in db.Events
+                                   where p.EventId == id
+                                   select p).FirstOrDefault();
+
+                db.Remove(evento);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        // ---------------------------------- CreatePublication ----------------------------------
+        [HttpGet]
+        public IActionResult CreatePublication()
+        {
+            using MuseumContext db = new();
+
+            Publication Modello = new();
+
+            return View("CreatePublication", Modello);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreatePublication(Publication formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                using MuseumContext db = new();
+
+                return View("CreatePublication", formData);
+
+            }
+            using (MuseumContext db = new MuseumContext())
+            {
+
+                db.Publications.Add(formData);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        // ---------------------------------- UpdatePublication ----------------------------------
+        [HttpGet]
+        public IActionResult UpdatePublication(int id)
+        {
+            using (MuseumContext db = new MuseumContext())
+            {
+                Publication publication = db.Publications.Where(p => p.PublicationId == id).FirstOrDefault();
+
+                if (publication == null)
+                {
+                    return NotFound("La publicazione cercata non è disponibile");
+                }
+
+                Publication Modello = new();
+
+                return View("UpdatePublication", Modello);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdatePublication(Publication formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                using MuseumContext db = new();
+
+                return View("UpdatePublication", formData);
+            }
+
+            using (MuseumContext db = new MuseumContext())
+            {
+                Publication publication = db.Publications.Where(p => p.PublicationId == formData.PublicationId).FirstOrDefault();
+
+                if (publication != null)
+                {
+                    publication.Title = formData.Title;
+                    publication.Description = formData.Description;
+                    publication.PhotoUrl = formData.PhotoUrl;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("La publicazione non è stato trovato");
+                }
+
+            }
+
+        }
+
+        // ---------------------------------- RemovePublication ----------------------------------
+        [HttpPost]
+        public IActionResult RemovePublication(int id)
+        {
+
+            using (MuseumContext db = new MuseumContext())
+            {
+                Publication publication = (from p in db.Publications
+                                where p.PublicationId == id
+                                select p).FirstOrDefault();
+
+                db.Remove(publication);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
     }
 }
